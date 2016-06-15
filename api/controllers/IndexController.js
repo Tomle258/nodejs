02b3435a56ea9085c57d198d88b23b40bbdc8038
Tomle <tuanvu060591@gmail.com>
2016-06-15 17:06:51 +0700

    my first commit

--- api/controllers/IndexController.js
+++ api/controllers/IndexController.js
@@ -1,5 +1,6 @@
 module.exports = {
   index: function(req, res) {
-    return res.view('Front_end/index',{title: 'Template reponsive html5 jquery đẹp'});
+  	return res.send('123');
+    //return res.view('Front_end/index',{title: 'Template reponsive html5 jquery đẹp'});
   }
 };
